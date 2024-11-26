38b7612567e45c078b89de3acdc442efda8cfd22
ricardomzacarias <rlmz0327@gmail.com>
2024-11-26 08:02:26 +0100

    first commit

--- python/kata_8/is_it_even.py
+++ python/kata_8/is_it_even.py
@@ -14,10 +14,10 @@ Floats with decimal part non equal to zero are considered UNeven for this kata.
 def is_even(n):
 # consultamos si el valor es un integer
     if isinstance(n, int):
-# si el resto es mayor que 0 es par y retornamos True
+# si el resto es igual que 0 es par y retornamos True
         if n%2==0:
             return True
-# en este challenge se nos pide evaluar si es float, es directamente "False"
+# en este challenge se nos pide evaluar, si es float es directamente "False"
     elif isinstance(n, float):
 # evaluamos si es par retornamos false
         if n%2==0:
@@ -25,3 +25,7 @@ def is_even(n):
 # para el resto de valores podemos retornar un false, ya que no pasar cadenas de caracteres
     else:
         return False
+
+# BEST KATA
+def is_eve(n):
+    return n%2==0
